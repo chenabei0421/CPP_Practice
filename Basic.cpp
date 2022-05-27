eed37e77b9f9c1f7b8c11aa1d2f7d782d4a7f152
cowbadma <cowbadma@gmail.com>
2022-05-27 18:33:30 +0800

    BinarySearch

---
+++ Basic.cpp
@@ -0,0 +1,32 @@
+// print, input
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;  
+int main( ) {  
+  int age;  
+   cout << "Enter your age: ";  
+   cin >> age;  
+   cout << "Your age is: " << age << endl;
+
+   char ary[] = "Welcome to C++ tutorial";  
+   cout << "Value of ary is: " << ary << endl;  
+}
+
+
+// Pointer
+
+
+// Structure: struct size
+
+
+// Bitwise Operation
+// const, static, volatile, union function
+// enum
+// https://www.geeksforgeeks.org/c-multiple-choice-questions/
+
+//OO
+// 物件
+// 封裝
+// 多型
